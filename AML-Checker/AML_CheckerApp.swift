b639117b391d9afcc99acc23ab0300ed12183855
Kirill_Aksenov <aksenovki@gmail.com>
2025-07-08 14:50:42 +0300

    Initial Commit

---
+++ AML-Checker/AML_CheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AML_CheckerApp.swift
+//  AML-Checker
+//
+//  Created by Kirill Aksenov on 08.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct AML_CheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
